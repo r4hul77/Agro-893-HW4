Fixed Road Trip Efficiency function btw good job on UAV units look perfect

Cars_and_UAVs.py
@@ -14,7 +14,7 @@ from vehicles import Car, UAV
 """       SYSTEM STUFF        """
 """***************************"""
 # Make a superclass for vehicles
-
+np.random.seed(0)
 def main():
 
     # Trips
@@ -22,7 +22,7 @@ def main():
     trips = [Road_trip(WayPoints= np.linspace(0, 30, length_trips), SpeedLims=np.random.rand(length_trips)*105,
                        Roughness=np.random.uniform(low=0.3, high=0.8, size=length_trips)),
              Flight_plan(WayPoints= np.linspace(0, 1, length_trips), Headwinds=np.random.rand(length_trips)*18),
-             Road_trip(WayPoints= np.linspace(0, 30, length_trips), SpeedLims=np.random.rand(length_trips)*105,
+             Road_trip(WayPoints= np.linspace(0, 300, length_trips), SpeedLims=np.random.rand(length_trips)*105,
                        Roughness=np.random.uniform(low=0.3, high=0.8, size=length_trips)),
              Flight_plan(WayPoints=np.linspace(0, 200, length_trips), Headwinds=np.random.rand(length_trips) * 18)]
 

trips.py
@@ -9,6 +9,7 @@ class Trip(object):
         self.way_points = None
         self.time = None
         self.speed = None
+        self.distances = None
 
     def set_start(self, start):
         self.start = start
@@ -19,9 +20,14 @@ class Trip(object):
 
     def set_way_points(self, way_points):
         self.way_points = way_points
+        self.distances = np.cumsum(way_points)
 
     def run(self):
         pass
+
+    def get_idx(self, loc):
+        idx = np.searchsorted(self.distances, loc)
+        return min(idx, len(self.distances) - 1)
 
 
 class Road_trip(Trip):
@@ -61,8 +67,9 @@ class Road_trip(Trip):
 
         """ Here is placeholder code until you get that done.  It 
             assumes that Loc falls in the first interval         """
-        Speed = self.SpeedLimits[0]  # Use the interval's speed limit
-        Efficiency = E_effic * self.Roughness[0]  # Use self.E_effic * Roughness
+        idx = self.get_idx(Loc)
+        Speed = self.SpeedLimits[idx]  # Use the interval's speed limit
+        Efficiency = E_effic * self.Roughness[idx]  # Use self.E_effic * Roughness
 
         # Done
         return Speed, Efficiency